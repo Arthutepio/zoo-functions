9660ec545d737e90df2b93d997d2f7f7e74623c7
Arthutepio <arthurodriguesr10@gmail.com>
2022-07-28 21:59:28 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  const { residents } = data.species.find(({ name }) => name === animal)
+  const { residents } = data.species.find(({ name }) => name === animal);
   return residents.every((element) => element.age > age);
 }
 
