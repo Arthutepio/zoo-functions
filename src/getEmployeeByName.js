624f8f7ccf84db9d1c0102c2f89dc99c39ae6597
Arthutepio <arthurodriguesr10@gmail.com>
2022-07-29 02:50:53 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,9 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) return {};
+  return data.employees.find((nomes) => nomes.firstName === employeeName
+  || nomes.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
