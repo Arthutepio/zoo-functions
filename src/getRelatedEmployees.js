633839e1fed8837e7db5fac1b033acdaa7aab49d
Arthutepio <arthurodriguesr10@gmail.com>
2022-07-29 17:26:35 -0300

    Realizei parte do exercicio 4 e 7, ambos precisamser concluídos

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,6 +2,7 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  return data.employees.some((element) => element.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,24 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+const { averageAge } = handlerElephants;
+console.log(averageAge);
+describe('Testes da função HandlerElephants', () => {
+  it('retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('retorna um array com a relação dos nomes de todos os elefantes', () => {
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+  });
+  it('retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+//   it('', () => {
+//     expect(handlerElephants()).to();
+//   });
+//   it('', () => {
+//     expect(handlerElephants()).to();
+//   });
+//   it('', () => {
+//     expect(handlerElephants()).to();
+//   });
+});
