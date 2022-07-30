30b126a2fd6df46146739aa2bee9f93a32195663
Arthutepio <arthurodriguesr10@gmail.com>
2022-07-30 16:45:02 -0300

    requisitos 6, 7 e 15 finalizados, todos eles são referentes a teste

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,13 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const obj = {};
+  if (animal === undefined) {
+    data.species.forEach((specie) => {
+      obj[specie.name] = specie.residents.length;
+    });
+    return obj;
+  }
 }
-
+console.log(countAnimals());
 module.exports = countAnimals;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -4,21 +4,37 @@ const { averageAge } = handlerElephants;
 console.log(averageAge);
 describe('Testes da função HandlerElephants', () => {
   it('retorna a quantidade de elefantes', () => {
-    expect(handlerElephants('count')).toBe(4);
+    const actual = handlerElephants('count');
+    const expected = 4;
+    expect(actual).toBe(expected);
   });
   it('retorna um array com a relação dos nomes de todos os elefantes', () => {
-    expect(handlerElephants('names').includes('Jefferson')).toBeTruthy();
+    const actual = handlerElephants('names').includes('Jefferson');
+    expect(actual).toBeTruthy();
   });
   it('retorna a média de idade dos elefantes', () => {
-    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+    const actual = 'averageAge';
+    const expected = 10.5;
+    expect(handlerElephants(actual)).toBeCloseTo(expected);
   });
-//   it('', () => {
-//     expect(handlerElephants()).to();
-//   });
-//   it('', () => {
-//     expect(handlerElephants()).to();
-//   });
-//   it('', () => {
-//     expect(handlerElephants()).to();
-//   });
+  it('Não passando argumentos a função deve retornar undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  it('Para o argumento popularity deve retornar um número igual ou maior a 5', () => {
+    const actual = handlerElephants('popularity');
+    const expected = 5;
+    expect(actual).toBeGreaterThanOrEqual(expected);
+  });
+  it('Para o argumento availability deve retornar um array de dias da semana que não contém Monday', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+  it('Para o argumento availability deve retornar um array de dias da semana que não contém Monday', () => {
+    expect(handlerElephants('location')).toEqual('NW');
+  });
+  it('Passando por argumento um objeto vazio ({}) deve retornar a string `Parâmetro inválido, é necessário uma string`', () => {
+    expect(handlerElephants({})).toMatch('Parâmetro inválido, é necessário uma string');
+  });
+  it('Passada uma string que não contempla uma funcionalidade deve retornar null', () => {
+    expect(handlerElephants('Qualquer coisa')).toBeNull();
+  });
 });
