0927d9df2c2da14d95e4ba25d7e9130c6a1df5a1
Arthutepio <arthurodriguesr10@gmail.com>
2022-07-30 22:15:31 -0300

    requisito 5 finalizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,13 +2,20 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
-  const obj = {};
+  let obj = {};
   if (animal === undefined) {
     data.species.forEach((specie) => {
       obj[specie.name] = specie.residents.length;
     });
     return obj;
   }
+  if (animal.specie && !animal.sex) {
+    obj = data.species.find((specie) => specie.name === animal.specie).residents.length;
+  } else {
+    obj = data.species.find((specie) => specie.name === animal.specie).residents
+      .filter(({ sex }) => sex === animal.sex).length;
+  }
+  return obj;
 }
 console.log(countAnimals());
 module.exports = countAnimals;
