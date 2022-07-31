69a39c60268f8cb831fd1fbe3e9f26194a7f97f0
Arthutepio <arthurodriguesr10@gmail.com>
2022-07-30 23:04:56 -0300

    atualizando o exercicio 5

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,10 @@
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
-}
 
+}
+data.hours.filter((hr) => console.log(hr.Tuesday));
+console.log(getSchedule('lions'));
 module.exports = getSchedule;
